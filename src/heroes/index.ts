c942d31aad08bea60206541b68ea60274c38bc3a
Lucía Vásquez <bvlucia34@gmail.com>
2023-08-09 21:08:00 -0600

    add query params

--- src/heroes/index.ts
+++ src/heroes/index.ts
@@ -1,6 +1,7 @@
 export * from './components';
 export * from './data';
 export * from './helpers';
+export * from './hooks';
 export * from './interfaces';
 export * from './pages';
 export * from './routes';
--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,6 +1,27 @@
-// import { HeroCard } from "../components";
+import queryString from "query-string";
+import { HeroCard } from "../components";
+import { useForm } from "../hooks";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const SearchPage = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const {q = ''} = queryString.parse(location.search);
+    console.log(q);
+
+    const {searchText, onInputChange} = useForm({
+        searchText: ''
+    });
+
+    const onSearchSubmit = ( event: any ) => {
+        event.preventDefault();
+        if (searchText.trim().length <= 1) return;
+
+        navigate(`?q=${searchText.toLowerCase().trim()}`)
+    }
+
     return (
     <>
         <h1>Search</h1>
@@ -10,13 +31,15 @@ export const SearchPage = () => {
         <div className="col-5">
             <h4>Searching</h4>
             <hr />
-            <form>
+            <form onSubmit={ onSearchSubmit }>
             <input 
                 type="text"
                 placeholder="Search a hero"
                 className="form-control"
                 name="searchText"
                 autoComplete="off"
+                value={ searchText }
+                onChange={ onInputChange }
             />
 
             <button type="submit" className="btn btn-outline-primary mt-1">
@@ -34,7 +57,7 @@ export const SearchPage = () => {
             </div>
 
             <div className="alert alert-danger">
-            No hero with <b>ABC</b>
+            No hero with <b>{q}</b>
             </div>
 
             {/* <HeroCard /> */}
