a240d486349be8997fac34ac5ca64970e4c52d8f
Lucía Vásquez <bvlucia34@gmail.com>
2023-08-10 21:35:52 -0600

    cambiando titulo de home

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
                 className="navbar-brand" 
                 to="/"
             >
-                Asociaciones
+                Home
             </Link>
 
             <div className="navbar-collapse">
