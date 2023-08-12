160717665a0ea1dd9d31ab4d303a93da90f1593f
Lucía Vásquez <bvlucia34@gmail.com>
2023-08-11 20:27:02 -0600

    cambiando nombre a carpeta assets a public

--- src/heroes/components/HeroCard.tsx
+++ src/heroes/components/HeroCard.tsx
@@ -13,7 +13,7 @@ const CharactersByHero = ({alter_ego, characters}: CharactersProps) => {
 
 export const HeroCard = (hero: Hero) => {
 
-    const heroImageUrl: string = `/assets/heroes/${hero.id}.jpg`;
+    const heroImageUrl: string = `/heroes/${hero.id}.jpg`;
 
     return (
     <div className="col animate__animated animate__fadeIn">
